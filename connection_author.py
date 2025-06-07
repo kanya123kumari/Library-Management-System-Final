b7b2244e533b05b4016799ffca12ea91aa6ac3a7
kanya123kumari <kanya.kumari005@gmail.com>
2025-06-07 13:04:43 +0530

    Add files via upload

---
+++ connection_author.py
@@ -0,0 +1,141 @@
+import mysql.connector
+
+host = 'localhost'
+user = 'root'
+password = 'root'
+database = 'library'
+port = '3306'
+
+# connection to MYSQL using keyword arguments
+
+conn = mysql.connector.connect(host=host,user=user,password=password,database=database,port=port)
+print(conn)
+cursor = conn.cursor()
+
+if conn:
+    print("connection successfull")
+
+else:
+    print("Try again")
+
+# Inserting data into the employee table(C - Create)
+
+def add_authors(authorID, name, country): 
+    try:
+        cursor1 = conn.cursor()
+        conn.start_transaction()  # Begin transaction
+        
+        query = "insert into library.authors(authorID, name, country) values (%s, %s, %s)"
+        cursor1.execute(query, (authorID, name, country))
+        
+        conn.commit()  # Commit transaction
+        print("Authors data added successfully")
+
+    except Exception as e:
+        conn.rollback()  # Rollback the transaction in case of an error
+        print("Transaction failed:", e)
+
+    finally:
+        cursor1.close()
+
+# view the employees table(R - Read)
+def get_authors():
+    try:
+        cursor2 = conn.cursor()
+        query = "select * from authors"
+        cursor2.execute(query)
+
+        for row in cursor2.fetchall():
+            print(row)
+
+    except Exception as e:
+        print("Data not visible: ",e)
+    finally:
+        cursor2.close()
+
+# Update the employees record(U - update)
+
+def update_authors(authorID, name):
+    print("--------update operation---------")
+
+    try:
+        cursor3 = conn.cursor()
+        conn.start_transaction()
+        
+        query = "update authors set name = %s where authorID = %s"
+        cursor3.execute(query, (name, authorID))
+
+        conn.commit()
+        print("Data updated successfully")
+
+    except Exception as e:
+        conn.rollback()
+        print("Update failed:", e)
+
+    finally:
+        cursor3.close()
+
+# Delete a record from employees
+
+def delete_authors(authorID):
+    print("-------Delete operation-------")
+
+    try:
+        cursor4 = conn.cursor()
+        conn.start_transaction()
+        query = "delete from authors where authorkID=%s"
+        cursor4.execute(query,(authorID))
+        conn.commit()
+        print("Data deleted successfully")
+        cursor4.close()
+
+    except Exception as e:
+        conn.rollback()
+        print("Delete failed: ",e)
+    finally:
+        cursor4.close()
+
+while True:
+    print("Choose an operation: ")
+    print("1 Add authors")
+    print("2 Update authors name")
+    print("3 Delete authors")
+    print("4 View authors")
+    print("5 Exit")
+
+    userInput = int(input("Enter your choice: "))
+    if userInput == 1:
+        print("-------insert operation--------")
+        authorID = int(input("Enter authors id: "))
+        name = input("Enter authors name: ")        
+        country = input("Enter the country name: ")
+        add_authors(authorID, name, country)
+        break
+
+    elif userInput ==2:
+        print("----------Update author name----------")
+        authorID =int(input("Enter the author id: "))
+        name = input("Enter authors name:")
+        update_authors(authorID,name)
+        break
+
+    elif userInput ==3:
+        print("---------Delete operation----------")
+        authorID = int(input("Enter the author id: "))
+        delete_authors(authorID)
+        break
+
+    elif userInput == 4:
+        print("-------Read operation-----------")
+        get_authors()
+        break
+
+    elif userInput == 5:
+        print("Exit from program...")
+        break
+    else:
+        print("Wrong user input.")
+        break
+
+        conn.close()
+        print("Database connection closed.")
